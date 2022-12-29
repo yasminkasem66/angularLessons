6c09ae11411ea63556533c030b95c23d1e1bbee4
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-12-29 13:21:09 +0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NewCourseFOrmComponent } from './new-course-form/new-course-form.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LikesComponent } from './likes/likes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LikesComponent, pathMatch: 'full' },
+  { path: 'cource', component: NewCourseFOrmComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/validators/password.validator.ts
@@ -0,0 +1,18 @@
+import { FormGroup } from '@angular/forms';
+export function validatePassword(control1: string, control2: string) {
+    return (control: FormGroup): { [key: string]: boolean } | null => {
+
+        const passwrd = control.controls[control1];
+        const confirmpasswrd = control.controls[control2];
+
+        if (passwrd.value !== confirmpasswrd.value) {
+            confirmpasswrd.setErrors({ wrongPassword: true })
+        }
+
+        return null
+
+    }
+}
+
+
+// validatePassword('password','confirmPassword')
